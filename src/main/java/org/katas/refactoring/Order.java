102d8fee2e0a14093694bf5c4f9df5dc9db9e94b
sdp <403213485@qq.com>
2019-08-27 20:29:43 +0800

    Order

--- src/main/java/org/katas/refactoring/Order.java
+++ src/main/java/org/katas/refactoring/Order.java
@@ -3,25 +3,25 @@ package org.katas.refactoring;
 import java.util.List;
 
 public class Order {
-    String nm;
-    String addr;
-    List<LineItem> li;
+    String name;
+    String address;
+    List<LineItem> lineList;
 
-    public Order(String nm, String addr, List<LineItem> li) {
-        this.nm = nm;
-        this.addr = addr;
-        this.li = li;
+    public Order(String name, String address, List<LineItem> lineList) {
+        this.name = name;
+        this.address = address;
+        this.lineList = lineList;
     }
 
     public String getCustomerName() {
-        return nm;
+        return name;
     }
 
     public String getCustomerAddress() {
-        return addr;
+        return address;
     }
 
     public List<LineItem> getLineItems() {
-        return li;
+        return lineList;
     }
 }
